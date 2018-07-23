ade793242942c64ca6e7588d8b26fae93590cc69
sunghyun kent lee <shkentlee@navercorp.com>
2018-07-23 14:25:20 +0900

    merge

--- src/main/java/com/medianote/herovote/vote/model/Vote.java
+++ src/main/java/com/medianote/herovote/vote/model/Vote.java
@@ -7,9 +7,20 @@ import lombok.Data;
 @Data
 public class Vote {
 	private long id;
+	
+	@NotEmpty(message = "이름을 입력 해주세요")
 	private String userName;
+	
+	@NotEmpty(message = "연락처를 입력 해주세요")
+	@Pattern(regexp = "(0-9)+", message = "숫자 0~9 까지만 입력 가능 합니다.")
 	private String cellPhone;
-	private String birthday;
+	
+	@NotEmpty(message = "생년월일을 입력 해주세요")
+	private String birthDay;
+	
 	private String voteYmd;
+	
+	@NotEmpty
+	@Size(min = 1, max = 3, message = "최소 1 이상 3 이하로 선택 해주세요")
 	private List<Hero> heros;
 }
